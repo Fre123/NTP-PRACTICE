4a4280714af45a398903fbe93c58c5261328fef0
Fre123 <johan.062@hotmail.com>
2016-11-23 15:30:44 -0500

    Adding modal-primary to the post

--- frontend/views/post/index.php
+++ frontend/views/post/index.php
@@ -21,7 +21,7 @@ $this->params['breadcrumbs'][] = $this->title;
     </button>
 
     <!-- Modal -->
-    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal modal-primary" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
